f4d4be798e3e5259fb31db67d048076df9df0759
farnazniazi <farnaz.niazi@gmail.com>
2021-08-21 21:15:10 -0400

    homework unit19

--- js/index.js
+++ js/index.js
@@ -1,1 +1,45 @@
-console.log
+console.log("your index.js file is loaded correctly.");
+
+    $(".img1").on("mouseenter", function(){
+      $(".img1").animate({
+        width: '75%'
+      });
+    });
+
+      $(".img1").on("mouseleave", function(){
+        $(".img1").animate({
+          width: '100%'
+        });
+    });
+    $(".img2").on("mouseenter", function(){
+      $(".img2").animate({
+        width: '75%'
+      });
+    });
+
+      $(".img2").on("mouseleave", function(){
+        $(".img2").animate({
+          width: '100%'
+        });
+    });$(".img3").on("mouseenter", function(){
+      $(".img3").animate({
+        width: '75%'
+      });
+    });
+
+      $(".img3").on("mouseleave", function(){
+        $(".img3").animate({
+          width: '100%'
+        });
+    });$(".img4").on("mouseenter", function(){
+      $(".img4").animate({
+        width: '75%'
+      });
+    });
+
+      $(".img4").on("mouseleave", function(){
+        $(".img4").animate({
+          width: '100%'
+        });
+    });
+  
